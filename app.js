8e5edd1b083d756c1f498be94c2de5edf5148677
Steven NEugent <sjneugent@protonmail.ch>
2019-03-15 14:36:24 -0400

    Still initial commit, but with app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let uploadRouter =require('./routes/upload');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/upload', uploadRouter);
+
+module.exports = app;
